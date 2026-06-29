87523888bc8db92f08671ad2fa77cf3b8a9cabc6
Kamyar Khorram <kamyar.khorram@yahoo.com>
2026-06-29 03:55:57 +0330

    Added a specific app name and named URL patterns to website/urls.py; updated base template to use dynamic URL tags for navigation links

--- website/urls.py
+++ website/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 # from mysite.views import import http_test
 # from .views import import http_test
 from website.views import *
+
+app_name = 'website' # A connector between the interpreter and urls that this app contain (In some circumstances, when projects are more advanced, Name similarity between apps may occur)
+
 urlpatterns = [
     # Path ('url address', 'view', name)
     # path('home', index_view),
-    path('', index_view), # => Main Page
-    path('about', about_view),
-    path('contact', contact_view)
+    path('', index_view, name='index'), # => Main Page
+    path('about', about_view, name='about'),
+    path('contact', contact_view, name='contact')
 ]

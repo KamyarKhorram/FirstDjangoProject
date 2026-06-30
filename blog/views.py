10a330a6535cf45c8cdc30a611d5e1486968b1af
Kamyar Khorram <kamyar.khorram@yahoo.com>
2026-06-30 03:44:22 +0330

    wire up blog-single page with dynamic context data:
    - Modified the blog_single view to pass dynamic content including title, content, and author to the template
    - Updated blog-single.html template to render dynamic data via Django template variables

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def blog_view(request):
     return render(request, 'blog/blog-home.html')
 
 
 def blog_single(request):
-    return render(request, 'blog/blog-single.html')
-
+    context = {'title': 'Cyrus can be a great destination for traveling in summer season!',
+               'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet'
+                          'atque corporis distinctio eum labore officiis perspiciatis'
+                          'reiciendis rem unde vero? Accusantium adipisci architecto'
+                          'at corporis delectus deleniti dolor ea eligendi explicabo,'
+                          'fugiat illo, iure labore minus mollitia nemo neque quia,'
+                          'recusandae ullam vel voluptatem! Ab, adipisci, magnam!'
+                          'Labore, odit ullam!',
+               'author': 'Kamyar Khorram'}
+    return render(request, 'blog/blog-single.html', context)
